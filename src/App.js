f61d0b4ce764bb071702d7ee144ad3434829724a
JuanluMC <juanluismontescalvo@gmail.com>
2020-12-30 20:54:27 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const username = useFormInput('');
+  const password = useFormInput('');
+
+  const [error, setError] = useState(null);
+
+  const handleLogin = () => {
+
+    axios.post('http://localhost:4000/api/v1/login', { username: username.value, password: password.value }).then(response => {
+      setError('Correcto');
+    }).catch(error => {
+      if (error.response.status === 401) setError(error.response.data.message);
+      else setError("Error");
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h1>Login example</h1>
+      <div>
+        <div>
+          <label for="user">Usuario</label>
+        </div>
+        <div>
+          <input id="user" type="text" {...username} />
+        </div>
+      </div>
+      <div style={{ marginTop: 10 }}>
+        <div>
+          <label for="pass">Contraseña</label>
+        </div>
+        <div>
+          <input id="pass" type="password" {...password} />
+        </div>
+      </div>
+      {error && <><small style={{ color: 'red' }}>{error}</small><br /></>}<br />
+      <input type="button" value='Login' onClick={handleLogin} /><br />
     </div>
   );
+}
+
+const useFormInput = initialValue => {
+  const [value, setValue] = useState(initialValue);
+ 
+  const handleChange = e => {
+    setValue(e.target.value);
+  }
+  return {
+    value,
+    onChange: handleChange
+  }
 }
 
 export default App;
